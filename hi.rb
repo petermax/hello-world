450b04e339047fb703046951a952fc2773da9cb7
PeterMax <petermax_30@gmx.co.uk>
2015-02-19 15:35:59 +0000

    another file

---
+++ hi.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "It is  now #{Time.now}"
+h = {"colors" => ["red", "blue", "green"],
+      "letters" => ["a", "b", "c" ]}
+#split array
+puts h.assoc("letters")
+puts h.assoc("colors")
+#other 
+puts "gin joint".length
+puts "Rick".index("c")
+puts 42.even?
